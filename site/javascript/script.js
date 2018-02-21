418f101a2221285043a795c0c98b61c7fa54b5f8
ethankclam <ethankclam@gmail.com>
2018-02-21 17:37:01 +0000

    addeventlistener

--- site/javascript/script.js
+++ site/javascript/script.js
@@ -3,12 +3,14 @@
 
 // console.log(document instanceof HTMLDocument);
 
-function sayHello () {
+function sayHello (event) {
+	this.textContent = "Said it!";
 	var name =
 		document.getElementById("name").value;
 		var message = "Hello " + name + "!";
 
-		document
-		.getElementById("content")
-		.textContent = message;
+		document.getElementById("content").textContent = message;
 };
+
+document.querySelector("button")
+	.addEventListener("click", sayHello);
