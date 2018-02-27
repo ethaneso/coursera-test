98832735a1efd1cf2b178c20d7ee69727d016349
ethankclam <ethankclam@gmail.com>
2018-02-27 23:22:59 +0000

    function

--- site/javascript/script.js
+++ site/javascript/script.js
@@ -4,22 +4,27 @@
 // console.log(document instanceof HTMLDocument);
 
 // object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstname = "Mark";
-company.ceo.favColor = "blue";
-console.log(company);
-console.log("Company CEO name is: "
-	+ company.ceo.firstname);
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstname = "Mark";
+// company.ceo.favColor = "blue";
+// console.log(company);
+// console.log("Company CEO name is: "
+	 // + company.ceo.firstname);
 
-console.log(company["name"]);
-company.$stock = 110;
+// console.log(company["name"]);
+// ompany.$stock = 110;
 
-console.log(company);
-console.log("Stock price is: " + company[stockPropNmae]);
+// console.log(company);
+// console.log("Stock price is: " + company[stockPropNmae]);
 
-
+function multiply(x, y) {
+	return x * y;
+}
+console.log(multiply(5, 3));
+multiply.version = "v.1.0.0";
+console.log(multiply.version);
 
 //event 
 function sayHello (event) {
